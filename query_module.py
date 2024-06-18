b8bda5bb87863299dc3afe2bd38ef4ad26537fdc
Bharath Hemachandran <bharathkumar.h@thoughtworks.com>
2024-06-18 05:10:03 +0200

    Added query_module.py

--- query_module.py
+++ query_module.py
@@ -2,23 +2,29 @@ import models.groq_model as groq_model
 from prompt_template import get_prompt
 from search_index import perform_search
 
+
 def get_user_question():
     return input("Enter your question: ")
+
 
 def rephrase_question(question):
     system_prompt = get_prompt("GROQ_SYSTEM_SIMPLIFY_QUESTION_PROMPT")
     return groq_model.get_response(question, system_prompt)
 
+
 def query_search_index(question):
     return perform_search(question)
+
 
 def generate_response(question, answers):
     system_prompt = get_prompt("GROQ_SYSTEM_CUSTOMER_SUPPORT_PROMPT")
     return groq_model.get_response(question + " " + answers, system_prompt)
 
+
 if __name__ == "__main__":
     question = get_user_question()
     rephrased_question = rephrase_question(question)
+    print(f"Rephrased Question: {rephrased_question}")
     answers = query_search_index(rephrased_question)
     response = generate_response(rephrased_question, answers)
-    print(response)
+    print(f"Response after index search:\n {response}")
